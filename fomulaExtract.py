ddca38067073faecd5526cfd0810364bdcc3c2df
wshjtuk <62067217+wshjtuk@users.noreply.github.com>
2020-12-26 01:33:44 +0800

    Update fomulaExtract.py

--- fomulaExtract.py
+++ fomulaExtract.py
@@ -1,8 +1,11 @@
 import cv2
 from identify import recognize
 import numpy as np
+
 LARGEST_NUMBER_OF_SYMBOLS = 500
 IMG_SIZE = 100
+
+
 def extract(img):
     """
     This function is used to extract a fomula from the image
@@ -37,13 +40,13 @@ def extract(img):
     secondNum = recognize(img[:, rightSymbol:leftEqual])[0]
     fomula = firstNum + symbol + secondNum + equal
 
-
     for h in range(height):
         for w in range(rightEqual, width):
             if img[h, w, 0] != 255:
                 ifAnswer = True
 
-    return((fomula, ifAnswer))
+    return ((fomula, ifAnswer))
+
 
 def fomulaExtract(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -51,22 +54,36 @@ def fomulaExtract(img):
 
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img, contours, -1, (0, 0, 255), 1)
-    print(len(contours))
-    dict = {}
-    for contour in contours:
+    cv2.imwrite('te.png', img)
+    leftSymbol = 0
+    rightSymbol = 0
+    num = 0
+    for contour in contours[1:]:
         x, y, w, h = cv2.boundingRect(contour)
-        dict[(x, y, w, h)] = x
-    dict = sorted(dict)
-    print(dict)
-    firstNumPos = dict[1]
-    symbolPos = dict[2]
-    secondNumPos = dict[3]
-    firstNum = img[firstNumPos[1]:firstNumPos[1] + firstNumPos[3], firstNumPos[0]:firstNumPos[0] + firstNumPos[2]]
-    symbol = img[symbolPos[1]:symbolPos[1] + symbolPos[3], symbolPos[0]:symbolPos[0] + symbolPos[2]]
-    secondNum = img[secondNumPos[1]:secondNumPos[1] + secondNumPos[3],
-                secondNumPos[0]:secondNumPos[0] + secondNumPos[2]]
-    print(recognize(firstNum))
-    recognize(symbol)
-    recognize(secondNum)
-img1 = cv2.imread('im0.png')
+        image = img[:, x:x + w]
+        reg = recognize(image)
+        if reg == '+' or reg == '-' or reg == 'x' or reg == 'd':
+            leftSymbol = x
+            rightSymbol = x + w
+            symbol = reg
+            break
+    for contour in contours[1:]:
+        x, y, w, h = cv2.boundingRect(contour)
+        image = img[:, x:x + w]
+        cv2.imwrite('test'+str(num)+'.png',image)
+        num += 1
+        reg = recognize(image)
+        print(reg)
+        if reg == '=':
+            leftEqual = x
+            rightEqual = x + w
+
+    firstNum = recognize(img[:, 0:leftSymbol])
+    print(firstNum)
+    secondNum = recognize(img[:, rightSymbol:leftEqual])
+    print(secondNum)
+
+
+img1 = cv2.imread('im3.png')
 fomulaExtract(img1)
+
